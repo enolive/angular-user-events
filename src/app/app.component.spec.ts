3b4129c26da73df967f842581849ffdc99c4cab9
Christoph Welcz <christoph@welcz.de>
2023-03-02 20:43:44 +0100

    ⚗️ included additional workaround

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,14 +4,17 @@ import userEvent from '@testing-library/user-event'
 
 describe('AppComponent', () => {
   it('works', async () => {
-    await render(AppComponent)
+    const view = await render(AppComponent)
 
     const button = screen.getByRole('button')
     await userEvent.click(button)
+    // workaround option 3: trigger detectChanges
+    // view.detectChanges()
+
     // workaround option 1: using fireEvent instead of userEvent fixes the test
     // fireEvent.click(button)
 
-    // workaround option 2: waitFor fixes the test
+    // workaround option 3: waitFor fixes the test
     // await waitFor(() => {
     expect(screen.getByRole('status')).toHaveTextContent('You pressed the button!')
     // })
